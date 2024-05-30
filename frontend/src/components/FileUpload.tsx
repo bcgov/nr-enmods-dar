1fdd59f16484e9734b24675dd98630fe4f7c8372
vmanawat <vmanawat@salussystems.com>
2024-05-30 15:15:23 -0700

    Made expandable list, added icons, progress bar, checkboxes

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -1,88 +1,254 @@
-import { Box, Button, ButtonGroup, Divider, List, ListItem, ListItemText } from "@mui/material";
-import React, {useState} from "react";
-import { FileUploader } from "react-drag-drop-files";
-import { DeleteRounded, UploadFile } from "@mui/icons-material";
-import "@/index.css"
-import { color } from "@mui/system";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  Grid,
+  LinearProgress,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material'
+import React, { useState } from 'react'
+import { FileUploader } from 'react-drag-drop-files'
+import {
+  DeleteRounded,
+  UploadFile,
+  CheckCircle,
+  Error,
+  ExpandMore,
+  ChevronRight,
+} from '@mui/icons-material'
+import '@/index.css'
 
-const styles = {
-    listItemBox: {
-        width: '200px'
-    }
-}
-
-const fileTypes = ["xlsx", "csv", "txt"]
+const fileTypes = ['xlsx', 'csv', 'txt']
 let selectedFiles: any[] = []
-
+let validationSuccess = {}
 
 function FileUpload() {
-    const [files, setFiles] = useState(null);
-    const handleChange = (files, index) => {
-        setFiles(files);
-        selectedFiles = Array.from(files)
-        
-        if (index != undefined) {
-            selectedFiles.splice(index, 1)
-            files = selectedFiles;
+  const [files, setFiles] = useState(null)
 
-            setFiles(files)
-            selectedFiles = Array.from(files)
-        }
+  const handleChange = (files, index) => {
+    setFiles(files)
+    selectedFiles = Array.from(files)
+
+    if (index != undefined) {
+      selectedFiles.splice(index, 1)
+      files = selectedFiles
+
+      setFiles(files)
+      selectedFiles = Array.from(files)
+    } else {
+      files = null
     }
+  }
 
-    const validateFile = (file) => {
-        
-    }
+  const validateFile = (file, index: number) => {
+    console.log(file)
+  }
 
-    return (
-        <div>
-            <div>
-                <FileUploader
-                    classes="custom-file-upload"
-                    fileOrFiles
-                    multiple={true}
-                    types={fileTypes}
-                    handleChange={handleChange}
-                    name="file" 
-                    hoverTitle="Click to browse for files to upload"
-                    children={
-                        <label className="upload-container">
-                            {/* <input className="file-input" accept=".xlsx, .csv, .txt" type="file" multiple></input> */}
-                            <div className="upload-children">
-                                <span><UploadFile width="32" height="32" fontSize="large"/></span>
-                                <span style={{fontSize:30}}>
-                                    Upload or drop files right here
-                                </span>
-                                <div style={{fontSize:15, textAlign: "center"}}>Accepted file types: .xlsx, .csv, .txt</div>
+  const validateAllFiles = (files) => {
+    console.log(files)
+  }
+
+  const [expandList, setExpandList] = useState(false)
+  const handleExpandList = () => {
+    setExpandList(!expandList)
+  }
+
+  const toggleCheckBoxes = () => {}
+
+  return (
+    <div>
+      <div>
+        <FileUploader
+          classes="custom-file-upload"
+          fileOrFiles
+          multiple={true}
+          types={fileTypes}
+          handleChange={handleChange}
+          name="file"
+          hoverTitle="Click to browse for files to upload"
+          children={
+            <label className="upload-container">
+              {/* <input className="file-input" accept=".xlsx, .csv, .txt" type="file" multiple></input> */}
+              <div className="upload-children">
+                <span>
+                  <UploadFile width="32" height="32" fontSize="large" />
+                </span>
+                <span style={{ fontSize: 30 }}>
+                  Upload or drop files right here
+                </span>
+                <div style={{ fontSize: 15, textAlign: 'center' }}>
+                  Accepted file types: .xlsx, .csv, .txt
+                </div>
+              </div>
+            </label>
+          }
+        />
+      </div>
+
+      <div className="file-drop-list">
+        <Grid container>
+          <Grid item xs={6}>
+            <Button sx={{ color: 'black' }} onClick={handleExpandList}>
+              {expandList ? <ExpandMore /> : <ChevronRight />}
+              <label>{selectedFiles.length + ' files selected'}</label>
+            </Button>
+          </Grid>
+
+          {expandList ? (
+            <Grid item xs={6}>
+              <FormControlLabel
+                sx={{ float: 'right' }}
+                onClick={toggleCheckBoxes}
+                value="end"
+                control={<Checkbox />}
+                label="Select All"
+                labelPlacement="end"
+                className={'selectAllEmailCheckbox'}
+              />
+            </Grid>
+          ) : (
+            ''
+          )}
+        </Grid>
+
+        {expandList && (
+          <div className="file-list">
+            <List>
+              {files && selectedFiles.length > 0
+                ? selectedFiles.map((file, index) => (
+                    <>
+                      <Box id={'file-' + index}>
+                        <Grid container>
+                          <Grid item xs={5}>
+                            <ListItem key={index}>
+                              <ListItemText
+                                key={index}
+                                primary={file.name}
+                                secondary={
+                                  (file.size / (1024 * 1024)).toFixed(2) + 'MB'
+                                }
+                              />
+                            </ListItem>
+                          </Grid>
+
+                          <Grid item xs={3}>
+                            <div
+                              style={{
+                                width: 'max-content',
+                                alignItems: 'center',
+                                margin: 'auto',
+                                paddingTop: '10px',
+                              }}
+                            >
+                              {validationSuccess ? (
+                                <Button style={{ color: 'green' }}>
+                                  <CheckCircle />
+                                </Button>
+                              ) : (
+                                <Button style={{ color: 'red' }}>
+                                  <Error />
+                                </Button>
+                              )}
+
+                              <Button
+                                style={{ color: 'black' }}
+                                onClick={() => {
+                                  handleChange(files, index)
+                                }}
+                              >
+                                <DeleteRounded />
+                              </Button>
                             </div>
-                        </label>
-                    }
-                />
-            </div>
-        
-            <div>
-                <List className="file-list">
-                    {files ? selectedFiles.map((file, index) => (
-                        <><ListItem key={index}>
-                            <ListItemText key={index} primary={file.name} secondary={(file.size / (1024*1024)).toFixed(2) + 'MB'}/>
-                            <Box>
-                                <ButtonGroup variant="text" style={{color:'black'}}>
-                                    <Button style={{color:'black'}} onClick={() => { validateFile(file); }}>Validate</Button>
-                                    <Button style={{color:'black'}}>Submit</Button>
-                                    <Button style={{color: 'black'}} onClick={() => { handleChange(files, index); }}><DeleteRounded/></Button>
-                                </ButtonGroup>
-                            </Box>
-                        </ListItem>
+                          </Grid>
 
-                        <Box><progress value={0.75}></progress></Box>
-                        <Divider component="li" variant="fullWidth"/>
-                        </>
-                    )) : "No files selected!"}
-                </List>
+                          <Grid item xs={4}>
+                            <FormControlLabel
+                              sx={{ float: 'right', paddingTop: '10px' }}
+                              value="end"
+                              control={<Checkbox />}
+                              label="Receive Email Conformation"
+                              labelPlacement="end"
+                              className={`emailCheckbox emailCheckbox-` + index}
+                            />
+                          </Grid>
+                        </Grid>
 
+                        <Grid container>
+                          <Grid
+                            item
+                            xs={9}
+                            sx={{ paddingLeft: '20px', paddingTop: '10px' }}
+                          >
+                            <LinearProgress
+                              color="secondary"
+                              variant="determinate"
+                              value={20}
+                              sx={{
+                                width: '90%',
+                                borderRadius: '5px',
+                                height: '15px',
+                              }}
+                            />
+                          </Grid>
+                          <Grid item xs={3}>
+                            <ButtonGroup
+                              variant="text"
+                              style={{ color: 'black', float: 'right', paddingBottom: '10px' }}
+                            >
+                              <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={() => {
+                                  validateFile(file, index)
+                                }}
+                              >
+                                Validate
+                              </Button>
+                              <Button variant="contained" color="secondary">
+                                Submit
+                              </Button>
+                            </ButtonGroup>
+                          </Grid>
+                        </Grid>
+                        <Divider component="li" variant="fullWidth" />
+                      </Box>
+                    </>
+                  ))
+                : ''}
+            </List>
+
+            <div className="all-file-action">
+              <Box sx={{paddingTop: '20px'}}>
+                {files && selectedFiles.length > 0 ? (
+                  <ButtonGroup variant="text" style={{ color: 'black' }}>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => {
+                        validateAllFiles(files)
+                      }}
+                    >
+                      Validate All
+                    </Button>
+                    <Button variant="contained" color="secondary">
+                      Submit All
+                    </Button>
+                  </ButtonGroup>
+                ) : (
+                  ''
+                )}
+              </Box>
             </div>
-        </div>
-    )
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default FileUpload
