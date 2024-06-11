cf5d3b68a0ae742bdaea71c18820899e6029fcb3
vmanawat <109625428+vmanawat@users.noreply.github.com>
2024-06-11 14:25:31 -0700

    fixing merge issue

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,18 +1,30 @@
 import { Routes, Route } from 'react-router-dom'
+import Roles from '../roles'
 import NotFound from '@/pages/NotFound'
 import Dashboard from '@/pages/Dashboard'
 import Admin from '@/pages/Admin'
 import HomePage from '@/pages/HomePage'
 import FileUpload from '@/pages/FileUpload'
+import { ProtectedRoutes } from './protected-routes'
 
 export default function AppRoutes() {
   return (
+    <>
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/admin" element={<Admin />} />
+      <Route element={<ProtectedRoutes roles={[Roles.ENMODS_ADMIN]} />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/submit" element={<FileUpload />} />
+
+      </Route>
+
+      <Route element={<ProtectedRoutes roles={[Roles.ENMODS_USER]} />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/submit" element={<FileUpload />} />
+      </Route>
+
       <Route path="*" element={<NotFound />} />
-      <Route path="/submit" element={<FileUpload />} />
     </Routes>
+  </>
   )
 }
