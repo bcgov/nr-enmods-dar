24c0192ee3aa248e1fcb1f8e92727b5a77bd00a3
mgtennant <100305096+mgtennant@users.noreply.github.com>
2024-06-12 10:59:09 -0700

    update db variable name to reflect .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,7 +22,7 @@ const DB_USER = process.env.POSTGRES_USER || "postgres";
 const DB_PWD = encodeURIComponent(process.env.POSTGRES_PASSWORD || "default"); // this needs to be encoded, if the password contains special characters it will break connection string.
 const DB_PORT = process.env.POSTGRES_PORT || 5432;
 const DB_NAME = process.env.POSTGRES_DATABASE || "postgres";
-const DB_SCHEMA = process.env.DB_SCHEMA || "users";
+const DB_SCHEMA = process.env.POSTGRES_SCHEMA || "users";
 const dataSourceURL = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
 
 function getMiddlewares() {
@@ -48,7 +48,7 @@ function getMiddlewares() {
         prismaOptions: {
           log: ["error", "warn"],
           errorFormat: "pretty",
-          datasourceUrl: `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`,
+          datasourceUrl: dataSourceURL,
         },
         middlewares: getMiddlewares(),
       },
