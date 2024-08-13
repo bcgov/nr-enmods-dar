82bc4b9af94bab11b78df929482f2d0f2e8c136e
mgtennant <100305096+mgtennant@users.noreply.github.com>
2024-08-13 13:36:53 -0700

    disable cron for now

--- backend/src/ftp/ftp.module.ts
+++ backend/src/ftp/ftp.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-// remove when EMSEDS-186 is merged as this will be done at App module level
 import { ScheduleModule } from "@nestjs/schedule";
 import { FtpService } from "./ftp.service";
 import { FtpController } from "./ftp.controller";
