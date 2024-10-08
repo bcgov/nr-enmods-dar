5a2161331ea04aabe815608cee070e5664b7cd86
barrfalk <bfalk@salussystems.com>
2024-10-08 11:51:54 -0700

    Moving to caddy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,17 +5,17 @@
 // templates directive to easily handle runtime variables
 
 const config = {
-  KEYCLOAK_CLIENT_ID: window.VITE_APP_KEYCLOAK_CLIENT_ID || import.meta.env.VITE_APP_KEYCLOAK_CLIENT_ID,
+  KEYCLOAK_CLIENT_ID: window.REACT_APP_KEYCLOAK_CLIENT_ID || import.meta.env.REACT_APP_KEYCLOAK_CLIENT_ID,
 
-  API_BASE_URL: window.VITE_APP_API_URL || import.meta.env.VITE_APP_API_URL,
+  API_BASE_URL: window.REACT_APP_API_URL || import.meta.env.REACT_APP_API_URL,
 
-  KEYCLOAK_URL: window.VITE_APP_KEYCLOAK_URL || import.meta.env.VITE_APP_KEYCLOAK_URL,
+  KEYCLOAK_URL: window.REACT_APP_KEYCLOAK_URL || import.meta.env.REACT_APP_KEYCLOAK_URL,
 
-  KEYCLOAK_REALM: window.VITE_APP_KEYCLOAK_REALM || import.meta.env.VITE_APP_KEYCLOAK_REALM,
+  KEYCLOAK_REALM: window.REACT_APP_KEYCLOAK_REALM || import.meta.env.REACT_APP_KEYCLOAK_REALM,
 
-  COMS_URL: window.VITE_APP_COMS_URL || import.meta.env.VITE_APP_COMS_URL,
+  COMS_URL: window.REACT_APP_COMS_URL || import.meta.env.REACT_APP_COMS_URL,
 
-  COMS_BUCKET: window.VITE_APP_COMS_BUCKET || import.meta.env.VITE_APP_COMS_BUCKET,
+  COMS_BUCKET: window.REACT_APP_COMS_BUCKET || import.meta.env.REACT_APP_COMS_BUCKET,
 }
 
 export default config
