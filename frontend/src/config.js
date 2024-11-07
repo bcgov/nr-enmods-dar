08e3ce40e6e49b87991763c76ac72e53bc54bad3
vmanawat <109625428+vmanawat@users.noreply.github.com>
2024-11-07 08:37:12 -0800

    updating env vars

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,10 +5,13 @@
 // templates directive to easily handle runtime variables
 
 const config = {
-  KEYCLOAK_CLIENT_ID: "enmods-5391",
-  API_BASE_URL: "api",
+  KEYCLOAK_CLIENT_ID: window.VITE_APP_KEYCLOAK_CLIENT_ID || import.meta.env.VITE_APP_KEYCLOAK_CLIENT_ID,
+
+  API_BASE_URL: window.VITE_APP_API_URL || import.meta.env.VITE_APP_API_URL,
+
   KEYCLOAK_URL: window.VITE_APP_KEYCLOAK_URL || import.meta.env.VITE_APP_KEYCLOAK_URL,
-  KEYCLOAK_REALM: "standard",
+
+  KEYCLOAK_REALM: window.VITE_APP_KEYCLOAK_REALM || import.meta.env.VITE_APP_KEYCLOAK_REALM,
 
   COMS_URL: window.REACT_APP_COMS_URL || process.env.REACT_APP_COMS_URL,
 
