6fd48ea4a38e08dfbc8aad5ab0e751f04191b14f
barrfalk <bfalk@salussystems.com>
2025-05-13 15:12:03 -0700

    Update excluded data.sql
    Added percentage of excluded data

--- data conversion/excluded data.sql
+++ data conversion/excluded data.sql
@@ -8,23 +8,21 @@ WITH raw_core_data AS (
          smpl.CLCT_METHD_CD
   FROM ems_samples smpl
     LEFT JOIN ems_results result ON smpl.id = result.smpl_id
-
-  left outer JOIN ems_monitoring_locations mloc ON smpl.mon_locn_id = mloc.id
-  LEFT JOIN ems.ems_locn_state_descriptor_export_map m 
-         ON mloc.locntyp_cd = m.type 
-        AND smpl.smpl_st_cd = m.state 
-        AND smpl.smpl_desc_cd = m.descriptor
-  LEFT JOIN OBSERVED_PROPERTIES_FOR_ETL ed
-         ON ed.Parm_code = result.parm_cd
-        AND ed.Analysis_Method_Code = result.anal_method_cd
-)
-SELECT
+    LEFT JOIN ems_monitoring_locations mloc ON smpl.mon_locn_id = mloc.id
+    LEFT JOIN ems.ems_locn_state_descriptor_export_map m 
+           ON mloc.locntyp_cd = m.type 
+          AND smpl.smpl_st_cd = m.state 
+          AND smpl.smpl_desc_cd = m.descriptor
+    LEFT JOIN OBSERVED_PROPERTIES_FOR_ETL ed
+           ON ed.Parm_code = result.parm_cd
+          AND ed.Analysis_Method_Code = result.anal_method_cd
+),
+excluded_counts AS (
+  SELECT
     COUNT(*) AS total_raw,
 
-    -- locntyp_cd filter
     SUM(CASE WHEN locntyp_cd LIKE 'D%' OR locntyp_cd LIKE 'P%' THEN 1 ELSE 0 END) AS excluded_due_to_locntyp,
 
-    -- medium filter
     SUM(CASE 
         WHEN NOT (
           UPPER(medium) LIKE '%WATER%' OR 
@@ -35,17 +33,40 @@ SELECT
         ) THEN 1 ELSE 0 
     END) AS excluded_due_to_medium,
 
-
-    -- continuous minimum condition for specific continuous data
     SUM(CASE 
         WHEN CLCT_METHD_CD IN ('CMON', 'C03', 'C01')
              AND (CONTINUOUS_MINIMUM IS NULL OR NewNameID IS NULL)
         THEN 1 ELSE 0 
     END) AS excluded_due_to_continuous,
-    
-    -- missing observed properties
+
     SUM(CASE 
         WHEN NewNameID IS NULL THEN 1 ELSE 0 
     END) AS excluded_due_to_missing_observed_property
 
-FROM raw_core_data
+  FROM raw_core_data
+)
+-- output counts with some basic stats
+SELECT 
+  total_raw,
+  excluded_due_to_locntyp,
+  excluded_due_to_medium,
+  excluded_due_to_continuous,
+  excluded_due_to_missing_observed_property,
+
+  -- Total of all exclusions (non-distinct)
+  excluded_due_to_locntyp +
+  excluded_due_to_medium +
+  excluded_due_to_continuous +
+  excluded_due_to_missing_observed_property AS total_excluded,
+
+  -- Percent excluded
+  ROUND(
+    (
+      excluded_due_to_locntyp +
+      excluded_due_to_medium +
+      excluded_due_to_continuous +
+      excluded_due_to_missing_observed_property
+    ) * 100.0 / total_raw, 
+    2
+  ) AS percent_excluded
+FROM excluded_counts;
