dbdae0182e5a42da589c889700b99ecb44a6e2f5
vmanawat <vmanawat@salussystems.com>
2025-06-09 14:56:49 -0700

    script to update units xref table

---
+++ migrations/sql/V1.2.2__update_unit_xref.sql
@@ -0,0 +1,14 @@
+INSERT INTO enmods.aqi_units_xref(
+        ems_code,
+        edt_unit_xref,
+        aqi_units_code,
+        create_user_id,
+        create_utc_timestamp,
+        update_user_id,
+        update_utc_timestamp
+    )
+values
+(NULL, 'g', 'g', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
+(NULL, 'mL', 'mL', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
+(NULL, 'uL/mL', 'µL/mL', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
+,(NULL, 'mg/dscm', 'mg/dscm', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
