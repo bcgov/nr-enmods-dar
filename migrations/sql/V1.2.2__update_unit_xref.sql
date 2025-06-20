cb23daa69c776846b833eb946cefeee406d9cc1c
vmanawat <vmanawat@salussystems.com>
2025-06-20 11:47:49 -0700

    updating the units xref table

--- migrations/sql/V1.2.2__update_unit_xref.sql
+++ migrations/sql/V1.2.2__update_unit_xref.sql
@@ -1,3 +1,11 @@
+DELETE FROM enmods.aqi_units_xref
+WHERE ems_code IN (11, 12, 34, 48, 058, 61, 62, 27, 263, 270, 310, 311, 49, 59, 213, 68);
+
+UPDATE enmods.aqi_units_xref
+SET edt_unit_xref = 'g'
+WHERE edt_unit_guid = 'b7d92d67-8935-4ee1-8a48-f2f70a7c1454';
+
+
 INSERT INTO enmods.aqi_units_xref(
         ems_code,
         edt_unit_xref,
@@ -8,7 +16,8 @@ INSERT INTO enmods.aqi_units_xref(
         update_utc_timestamp
     )
 values
-(NULL, 'g', 'g', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
-(NULL, 'mL', 'mL', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
-(NULL, 'uL/mL', 'µL/mL', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
-,(NULL, 'mg/dscm', 'mg/dscm', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
+(NULL, 'mL', 'mL', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc')),
+(NULL, 'uL/mL', 'µL/mL', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc')),
+(NULL, 'mg/dscm', 'mg/dscm', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc')),
+(NULL, 'tm3/d', 'tm³/d', 'VMANAWAT', (now() at time zone 'utc'), 'VMANAWAT', (now() at time zone 'utc'))
+
