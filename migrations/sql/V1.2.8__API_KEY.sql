21f20539e338482fb3e74ed342b07b73bc43437a
barrfalk <bfalk@salussystems.com>
2025-06-26 15:11:53 -0700

    Added API key funcionality

--- migrations/sql/V1.2.8__API_KEY.sql
+++ migrations/sql/V1.2.8__API_KEY.sql
@@ -1,7 +1,7 @@
 CREATE TABLE
-  api_keys (
+  enmods.api_keys (
     api_key_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-    api_key VARCHAR(255) NOT NULL UNIQUE,
+    api_key UUID NOT NULL UNIQUE DEFAULT gen_random_uuid (),
     username VARCHAR(200),
     email_address VARCHAR(200),
     organization_name VARCHAR(512),
@@ -11,7 +11,7 @@ CREATE TABLE
     revoked_by VARCHAR(512),
     enabled_ind BOOLEAN DEFAULT TRUE,
     create_user_id VARCHAR(200) NOT NULL,
-    create_utc_timestamp TIMESTAMP(6) NOT NULL,
+    create_utc_timestamp TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     update_user_id VARCHAR(200) NOT NULL,
-    update_utc_timestamp TIMESTAMP(6) NOT NULL
+    update_utc_timestamp TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
   );
