8dd496dfc46f92ac00fa1aaa829a9518af0e7a39
vmanawat <vmanawat@salussystems.com>
2025-11-14 11:46:08 -0800

    data population -- will revert

--- backend/src/cron-job/cron-job.service.ts
+++ backend/src/cron-job/cron-job.service.ts
@@ -363,7 +363,7 @@ export class CronJobService {
     }
   }
 
-  @Cron(CronExpression.EVERY_30_MINUTES)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   private async fetchAQSSData() {
     if (!this.operationLockService.acquireLock("PULLDOWN")) {
       this.logger.log(
