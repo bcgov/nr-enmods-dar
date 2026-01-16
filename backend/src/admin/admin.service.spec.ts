83bc9a3ce70acb96407a1b4e0a832726ea55ef1a
barrfalk <bfalk@salussystems.com>
2026-01-16 15:07:04 -0800

    Fixed jest tests

--- backend/src/admin/admin.service.spec.ts
+++ backend/src/admin/admin.service.spec.ts
@@ -1,18 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AdminService } from './admin.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AdminService } from "./admin.service";
+import { HttpService } from "@nestjs/axios";
 
-describe('AdminService', () => {
+describe("AdminService", () => {
   let service: AdminService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminService],
+      providers: [
+        AdminService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+            post: jest.fn(),
+            patch: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AdminService>(AdminService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
--- backend/src/file_submissions/file_submissions.controller.spec.ts
+++ backend/src/file_submissions/file_submissions.controller.spec.ts
@@ -1,20 +1,34 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { FileSubmissionsController } from './file_submissions.controller';
-import { FileSubmissionsService } from './file_submissions.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { FileSubmissionsController } from "./file_submissions.controller";
+import { FileSubmissionsService } from "./file_submissions.service";
+import { PrismaService } from "nestjs-prisma";
+import { ObjectStoreService } from "src/objectStore/objectStore.service";
+import { AqiApiService } from "src/aqi_api/aqi_api.service";
+import { OperationLockService } from "src/operationLock/operationLock.service";
+import { SanitizeService } from "src/sanitize/sanitize.service";
 
-describe('FileSubmissionsController', () => {
+describe("FileSubmissionsController", () => {
   let controller: FileSubmissionsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FileSubmissionsController],
-      providers: [FileSubmissionsService],
+      providers: [
+        FileSubmissionsService,
+        { provide: PrismaService, useValue: {} },
+        { provide: ObjectStoreService, useValue: {} },
+        { provide: AqiApiService, useValue: {} },
+        { provide: OperationLockService, useValue: {} },
+        { provide: SanitizeService, useValue: {} },
+      ],
     }).compile();
 
-    controller = module.get<FileSubmissionsController>(FileSubmissionsController);
+    controller = module.get<FileSubmissionsController>(
+      FileSubmissionsController,
+    );
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 });
